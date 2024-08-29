be6a0826d8f5b0c4566e16c9e2b156426e8455f0
otaki0413 <takida0413@gmail.com>
2024-08-30 04:03:35 +0900

    ✨ 管理サイトの表示カスタマイズ

--- ec/admin.py
+++ ec/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import Product, ProductImage
 
-admin.site.register(Product)
-admin.site.register(ProductImage)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ["name", "price", "created_at", "updated_at"]
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    model = ProductImage
+    readonly_fields = ("created_at", "updated_at")
