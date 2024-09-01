584369a6b516673fa3a2428352188a14e3cecfa2
otaki0413 <takida0413@gmail.com>
2024-09-01 18:55:02 +0900

    ✨ 詳細画面に商品データ表示

--- ec/views.py
+++ ec/views.py
@@ -1,4 +1,3 @@
-from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
@@ -14,3 +13,10 @@ class ProductListView(generic.ListView):
 class ProductDetailView(generic.DetailView):
     model = Product
     template_name = "ec/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["related_product_list"] = Product.objects.filter().order_by(
+            "-created_at"
+        )[:4]
+        return context
