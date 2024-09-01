5dc621b1d8322e978caee89a2760d57af0b6c757
otaki0413 <takida0413@gmail.com>
2024-09-01 17:43:24 +0900

    ✨ 商品詳細ページへのリンク追加

--- ec/urls.py
+++ ec/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "ec"
 urlpatterns = [
     path("products/", views.ProductListView.as_view(), name="product_list"),
-    path("products/<int:pk>", views.ProductDetailView.as_view(), name="product_detail"),
+    path(
+        "products/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"
+    ),
 ]
