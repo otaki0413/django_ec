ce9d58f9ddb3a206da0a2874d0174e379721bb50
otaki0413 <takida0413@gmail.com>
2024-09-13 19:57:21 +0900

    ✨ ルート(/)アクセス時に/productsにリダイレクトさせる

--- ec/urls.py
+++ ec/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = "ec"
 urlpatterns = [
+    path(
+        "",
+        RedirectView.as_view(pattern_name="ec:product_list"),
+        name="redirect_to_product_list",
+    ),
     path("products/", views.ProductListView.as_view(), name="product_list"),
     path(
         "products/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"
