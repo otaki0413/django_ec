c50508f7918bb0a52880a9f57e0036952a401047
otaki0413 <takida0413@gmail.com>
2024-09-20 23:33:50 +0900

    ✨ form_validメソッドのオーバーライドにより画像データの保存処理の追加

--- adminproducts/views.py
+++ adminproducts/views.py
@@ -16,12 +16,31 @@ class AdminProductListView(generic.ListView):
 class AdminProductRegisterView(generic.CreateView):
     model = Product
     form_class = ProductRegisterForm
-    second_form_class = ProductImageRegisterForm
+    image_form_class = ProductImageRegisterForm
     template_name = "adminproducts/new.html"
     success_url = reverse_lazy("adminproducts:admin_product_list")
 
     def get_context_data(self, **kwargs):
-        context = super(AdminProductRegisterView, self).get_context_data(**kwargs)
+        # 既存のコンテキストを取得
+        context = super().get_context_data(**kwargs)
+        # 画像フォームのインスタンスをコンテキストに設定
         if "image_form" not in context:
-            context["image_form"] = self.second_form_class()
+            context["image_form"] = self.image_form_class()
         return context
+
+    def form_valid(self, form):
+        """有効なフォームデータの場合に実行される処理"""
+        # Productモデルのインスタンスを保存
+        product = form.save()
+        # 画像フォームのデータとファイルを取得
+        image_form = self.image_form_class(self.request.POST, self.request.FILES)
+
+        if image_form.is_valid():
+            # 画像データを一旦取得し、関連するProductインスタンスを追加
+            product_image = image_form.save(commit=False)
+            product_image.product = product
+            # ProductImageモデルのインスタンスを保存
+            product_image.save()
+
+        # 親クラスのデフォルト処理（リダイレクトなど）を実行
+        return super().form_valid(form)
