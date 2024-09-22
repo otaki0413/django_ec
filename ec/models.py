eefb2b3e3011f6e4833e06e3283c7943f900e6e1
otaki0413 <takida0413@gmail.com>
2024-09-22 17:29:28 +0900

    🎨 アップロード画像の public_id 用のフィールド追加

--- ec/models.py
+++ ec/models.py
@@ -23,6 +23,7 @@ class ProductImage(models.Model):
         Product, on_delete=models.CASCADE, related_name="images"
     )
     image = models.ImageField(upload_to="images/", blank=True)
+    public_id = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField("登録日時", auto_now_add=True)
     updated_at = models.DateTimeField("更新日時", auto_now=True)
 
