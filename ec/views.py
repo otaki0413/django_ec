c5159f00e8b60c680311df4cb12e1040101812d0
otaki0413 <takida0413@gmail.com>
2024-09-28 22:58:48 +0900

    ✨ カート取得、カート商品追加機能の実装

--- ec/views.py
+++ ec/views.py
@@ -1,11 +1,8 @@
-import pprint
-
 from django.views import generic
 from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponseBadRequest
 
-from .models import Product
-from .cart import Cart
+from .models import Product, Cart, CartProduct
 
 
 class ProductListView(generic.ListView):
@@ -37,23 +34,44 @@ class CheckoutView(generic.TemplateView):
 def add_to_cart(request):
     """商品をカートに追加する処理"""
     if request.method == "POST":
-        # フォームデータから商品id取得
+        # フォームデータから商品ID取得
         product_id = request.POST.get("product_id")
         if not product_id:
             return HttpResponseBadRequest("商品が選択されていません。")
 
         # 商品の取得（存在しない場合404エラー）
         product = get_object_or_404(Product, pk=product_id)
-        # セッションからカート生成
-        cart = Cart.from_session(request.session, "cart")
-        # カートに商品追加
-        cart.add(product)
-        # セッションにカート保存
-        cart.save_session(request.session, "cart")
-        # デバッグ用
-        pprint.pprint(request.session["cart"])
+
+        # セッションキーの取得
+        session_key = request.session.session_key
+        # セッションキーが存在しない場合は、新規作成
+        if not session_key:
+            request.session.create()
+            session_key = request.session.session_key
+
+        # 既存カートの取得、なければ新規カートの作成
+        cart, _created = Cart.objects.get_or_create(session_key=session_key)
+
+        # カート商品の取得、なければ新規カート商品の作成
+        cart_product, created_product = CartProduct.objects.get_or_create(
+            cart=cart, product=product
+        )
+
+        # 数量の更新
+        if created_product:
+            # 新規商品をカートに追加する場合
+            cart_product.quantity = 1
+        else:
+            # 既存商品をカートに追加する場合
+            cart_product.quantity += 1
+
+        # カート商品の保存
+        cart_product.save()
 
         # 商品一覧ページにリダイレクト
         return redirect("ec:product_list")
 
     return HttpResponseBadRequest("無効なリクエストです。")
+
+
+# TODO:セッション有効期限が切れたカートの削除処理処理いる？
